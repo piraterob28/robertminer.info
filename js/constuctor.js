79c593167361286e1f00393a53d91b871372f781
piraterob28 <73079083+piraterob28@users.noreply.github.com>
2021-03-05 10:39:20 -0700

    Update constuctor.js

--- js/constuctor.js
+++ js/constuctor.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     // alert("the URL of this page is: " + window.location.pathname);
-    if (window.location.pathname.includes("liingo")){
+    if (window.location.pathname.includes("liiingo")){
         document.getElementById("hello").style.display = "flex";
     }
   };
